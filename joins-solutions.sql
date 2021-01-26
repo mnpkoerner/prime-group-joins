582b0ad7b8a91831e89335e3bc7eed78546e43fd
mnpkoerner <mnpkoerner@gmail.com>
2021-01-26 13:56:48 -0600

    most of basemode

--- joins-solutions.sql
+++ joins-solutions.sql
@@ -6,15 +6,31 @@ JOIN "addresses" ON "addresses".customer_id = "customers".id;
 
 -- 2. Get all orders and their line items (orders, quantity and product).
 
-SELECT * FROM "orders"
-JOIN "line_items" ON "line_items".order_id = "orders".id
-JOIN "products" ON "line_items".product_id = "products".id;
+SELECT "orders".id, "line_items".quantity, "products".description FROM "orders"
+LEFT JOIN "line_items" ON "line_items".order_id = "orders".id
+LEFT JOIN "products" ON "line_items".product_id = "products".id;
 
-3. Which warehouses have cheetos?
-4. Which warehouses have diet pepsi?
+-- 3. Which warehouses have cheetos?
+SELECT "warehouse".warehouse, "products".description FROM "warehouse"
+JOIN "warehouse_product" ON "warehouse_product".warehouse_id = "warehouse".id
+JOIN "products" ON "warehouse_product".product_id = "products".id
+WHERE "products".description LIKE 'cheetos';
+
+-- 4. Which warehouses have diet pepsi?
+SELECT "warehouse".warehouse, "products".description FROM "warehouse"
+JOIN "warehouse_product" ON "warehouse_product".warehouse_id = "warehouse".id
+JOIN "products" ON "warehouse_product".product_id = "products".id
+WHERE "products".description LIKE 'diet pepsi';
+
 5. Get the number of orders for each customer. NOTE: It is OK if those without orders are not included in results.
-6. How many customers do we have?
-7. How many products do we carry?
+
+
+-- 6. How many customers do we have?
+SELECT COUNT(*) AS "total_customers" FROM "customers";
+
+-- 7. How many products do we carry?
+SELECT COUNT(*) AS "total_products" FROM "products";
+
 8. What is the total available on-hand quantity of diet pepsi?
 
 ## Stretch
