e0f640627d800326451ff4ddab728258c89f5732
mnpkoerner <mnpkoerner@gmail.com>
2021-01-26 18:17:10 -0600

    some stretch

--- joins-solutions.sql
+++ joins-solutions.sql
@@ -22,7 +22,7 @@ JOIN "warehouse_product" ON "warehouse_product".warehouse_id = "warehouse".id
 JOIN "products" ON "warehouse_product".product_id = "products".id
 WHERE "products".description LIKE 'diet pepsi';
 
-5. Get the number of orders for each customer. NOTE: It is OK if those without orders are not included in results.
+-- 5. Get the number of orders for each customer. NOTE: It is OK if those without orders are not included in results.
 SELECT "customers".first_name, COUNT("orders".id) FROM "customers"
 JOIN "addresses" ON "addresses".customer_id = "customers".id
 JOIN "orders" ON "orders".address_id = "addresses".id
@@ -34,10 +34,20 @@ SELECT COUNT(*) AS "total_customers" FROM "customers";
 -- 7. How many products do we carry?
 SELECT COUNT(*) AS "total_products" FROM "products";
 
-8. What is the total available on-hand quantity of diet pepsi?
+-- 8. What is the total available on-hand quantity of diet pepsi?
+SELECT "products".description, SUM("warehouse_product".on_hand) FROM "products"
+JOIN "warehouse_product" ON "products".id = "warehouse_product".product_id
+JOIN "warehouse" ON "warehouse_product".warehouse_id = "warehouse".id
+WHERE "products".description LIKE 'diet pepsi'
+GROUP BY "products".description;
+
+-- ## Stretch
+-- 9. How much was the total cost for each order?
+SELECT "orders".id, SUM("products".unit_price) FROM "orders"
+JOIN "line_items" ON "line_items".order_id = "orders".id
+JOIN "products" ON "products".id = "line_items".product_id
+GROUP BY "orders".id;
 
 
-## Stretch
-9. How much was the total cost for each order?
 10. How much has each customer spent in total?
 11. How much has each customer spent in total? Customers who have spent $0 should still show up in the table. It should say 0, not NULL (research coalesce).
